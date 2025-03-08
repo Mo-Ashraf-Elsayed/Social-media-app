05e9727ee9d81350eb78d75838f105d34a075189
M-Ashraf-Elsayed <pop616631@gmail.com>
2025-03-09 00:01:07 +0200

    change tailwind to ng bootstrab

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgbCollapseModule, RouterLink, NgbDropdownModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   @Input() layout!: string;
+  isMenuCollapsed = true;
 }
